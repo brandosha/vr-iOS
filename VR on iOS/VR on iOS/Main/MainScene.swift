0c3febaaa349440c2435256acc0999f60e33e209
brandosha <33556661+brandosha@users.noreply.github.com>
2018-09-10 15:12:38 -0600

    Update MainScene.swift

--- VR on iOS/VR on iOS/Main/MainScene.swift
+++ VR on iOS/VR on iOS/Main/MainScene.swift
@@ -2,7 +2,7 @@
 //  MainScene.swift
 //  VR on iOS
 //
-//  Created by Emily on 9/3/18.
+//  Created by Brandon on 9/3/18.
 //  Copyright © 2018 Brandon. All rights reserved.
 //
 
@@ -18,6 +18,38 @@ class MainScene: VRScene {
     }
     
     func setup() {
+        
+        var ship = scene.rootNode.childNode(withName: "shipMesh", recursively: true)!
+        ship = VRObject(ship, type: .static)
+        
+        func highlight(object: VRObject) -> Bool {
+            
+            // get its material
+            let material = object.geometry!.firstMaterial!
+            
+            // highlight it
+            SCNTransaction.begin()
+            SCNTransaction.animationDuration = 0.5
+            
+            // on completion - unhighlight
+            SCNTransaction.completionBlock = {
+                SCNTransaction.begin()
+                SCNTransaction.animationDuration = 0.5
+                
+                material.emission.contents = UIColor.black
+                
+                SCNTransaction.commit()
+            }
+            
+            material.emission.contents = UIColor.red
+            
+            SCNTransaction.commit()
+            
+            return true
+            
+        }
+        
+        (ship as! VRObject).useObjectFunc = highlight
         
     }
     
